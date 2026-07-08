ed22aea1264a568153cc429d532271ff7296d1a9
Hamza-amnikon <hamza.rajmohammed@amnikontechnologies.com>
2026-07-08 22:20:17 +0530

    Images Changes

--- pages/careers/resume.js
+++ pages/careers/resume.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async function (e) {
         const formData = new FormData(form);
 
         const response = await fetch(
-            "https://wwwuat.amnikontechnologies.com/api/careers",
+            "http://wwwuat.amnikontechnologies.com/api/careers",
             {
                 method: "POST",
                 body: formData
